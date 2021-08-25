75d037829b757f4ee528fef85c6cdf4ea3f213b5
Paul Highum <phigh07@gmail.com>
2021-08-25 13:00:11 -0700

    Refactors logic out of Report component * Moves PUT req logic out of Report component into Reports component thereby following a better pattern of presentational components and container components * Changes button styling a little more

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,36 +1,6 @@
 import Button from './Button'
 
-const Report = ({ report, setReports, filterReports }) => {
-
-    // Put req to block report, returns all reports, filters out resolved, and resets state
-    const blockReport = async (reportId) => {
-        try {
-            const res = await fetch(`http://localhost:3000/reports/${reportId}/block`, {
-                method: 'PUT',
-                headers:{'Content-Type':'application/json'}
-            })
-            const data = await res.json()
-            const dataToDisplay = filterReports(data)
-            setReports(dataToDisplay)
-        } catch(err){
-            console.log(err)
-        }
-    }
-
-    // Put req to resolve report, returns all reports, filters out resolved, and resets state
-    const resolveReport = async (reportId) => {
-        try {
-            const res = await fetch(`http://localhost:3000/reports/${reportId}/resolve`, {
-                method: 'PUT',
-                headers:{'Content-Type':'application/json'}
-            })
-            const data = await res.json()
-            const dataToDisplay = filterReports(data)
-            setReports(dataToDisplay)
-        } catch(err){
-            console.log(err)
-        }
-    }
+const Report = ({ report, blockReport, resolveReport }) => {
 
     return (
         <div id={report.id} className='report'>
--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,14 +1,45 @@
 import Report from './Report'
 
 const Reports = ({ reports, setReports, filterReports }) => {
+
+    // Put req to block report, returns all reports, filters out resolved, and resets state
+    const blockReport = async (reportId) => {
+        try {
+            const res = await fetch(`http://localhost:3000/reports/${reportId}/block`, {
+                method: 'PUT',
+                headers:{'Content-Type':'application/json'}
+            })
+            const data = await res.json()
+            const dataToDisplay = filterReports(data)
+            setReports(dataToDisplay)
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+    // Put req to resolve report, returns all reports, filters out resolved, and resets state
+    const resolveReport = async (reportId) => {
+        try {
+            const res = await fetch(`http://localhost:3000/reports/${reportId}/resolve`, {
+                method: 'PUT',
+                headers:{'Content-Type':'application/json'}
+            })
+            const data = await res.json()
+            const dataToDisplay = filterReports(data)
+            setReports(dataToDisplay)
+        } catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <>
             {reports.map((report) => (
                 <Report 
                     key={report.id}
                     report={report}
-                    setReports={setReports}
-                    filterReports={filterReports}
+                    blockReport={blockReport}
+                    resolveReport={resolveReport}
                 />
             ))}
         </>
